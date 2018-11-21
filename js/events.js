3cbd8b9f67e96087ffbfb82cc41e2b3809a6f7d4
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-21 18:41:37 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,43 @@
 //define functions here
 
-$(document).ready(function(){
+//$('h1').on("eventname", function(){
+    //action you want taken
+//});
 
+function getIt() {
+    $('p').on('click', function() {
+      alert('Hey!');
+    });
+}
+
+function frameIt() {
+    $('img').on("load", function() {
+      $('img').addClass('tasty');
+    });
+}
+
+function submitIt() {
+    $('form').on('submit', function() {
+      alert('Your form is going to be submitted now.');
+    });
+}
+
+function pressIt() {
+    $('#typing').on('keydown', function(key) {
+      if(key.which == 71){
+        alert('g was pressed');
+      }
+  });
+}
+
+$(document).ready(function(){
 // call functions here
+  getIt();
+
+  frameIt();
+
+  submitIt();
+
+  pressIt();
 
 });
