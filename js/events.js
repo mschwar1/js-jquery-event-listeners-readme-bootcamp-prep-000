fe64377daf08b8c1f0653bd082d32d17cf0e1603
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-21 18:24:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,15 @@
 //define functions here
 
+$('h1').on("eventname", function(){
+    //action you want taken
+});
+
+function getIt() {
+    $('p').on("click", function()) {
+      alert('Hey!');
+    }
+}
+
 $(document).ready(function(){
 
 // call functions here
