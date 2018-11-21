198a84ec956c0bfeffd28f7666adff063854e109
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-21 18:41:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function submitIt() {
 }
 
 function pressIt() {
-    $('#typing').on('keydown', function() {
+    $('#typing').on('keydown', function(key) {
       if(key.which == 71){
         alert('g was pressed');
       }
